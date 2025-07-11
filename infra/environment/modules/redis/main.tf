1b9392f9043d431d1fee1a8d6ce64b9e48d63c86
ahmedabdelkhalek1 <medoooo163@gmail.com>
2025-07-11 17:32:43 +0300

    deploy the LB

--- infra/environment/modules/redis/main.tf
+++ infra/environment/modules/redis/main.tf
@@ -1,63 +1,15 @@
-# Create VPC Network
-resource "google_compute_network" "consumer_net" {
-  name                    = "my-network"
-  auto_create_subnetworks = false
+resource "google_redis_instance" "redis" {
+  name               = "demo-redis"
+  tier               = "STANDARD_HA"
+  memory_size_gb     = var.memory_size
+  region             = var.region
+  authorized_network = "projects/${var.project}/global/networks/custom-vpc"
 }
 
-# Create Subnet
-resource "google_compute_subnetwork" "consumer_subnet" {
-  name          = "my-subnet"
-  ip_cidr_range = "10.0.0.248/29"
-  region        = var.region
-  network       = google_compute_network.consumer_net.id
+output "redis_host" {
+  value = google_redis_instance.redis.host
 }
 
-# PSC Service Connection Policy for Memorystore
-resource "google_network_connectivity_service_connection_policy" "default" {
-  name          = "my-policy"
-  location      = var.region
-  service_class = "gcp-memorystore-redis"
-  description   = "my basic service connection policy"
-  network       = google_compute_network.consumer_net.id
-
-  psc_config {
-    subnetworks = [google_compute_subnetwork.consumer_subnet.id]
-  }
-}
-
-# Redis Cluster Resource
-resource "google_redis_cluster" "redis_cluster" {
-  name                        = "ha-cluster"
-  region                      = var.region
-  shard_count                 = 3
-  replica_count               = 1
-  node_type                   = "REDIS_SHARED_CORE_NANO" # change to a higher tier if needed
-  transit_encryption_mode     = "TRANSIT_ENCRYPTION_MODE_DISABLED"
-  authorization_mode          = "AUTH_MODE_DISABLED"
-  deletion_protection_enabled = true
-
-  redis_configs = {
-    maxmemory-policy = "volatile-ttl"
-  }
-
-  zone_distribution_config {
-    mode = "MULTI_ZONE"
-  }
-
-  psc_configs {
-    network = google_compute_network.consumer_net.id
-  }
-
-  depends_on = [
-    google_network_connectivity_service_connection_policy.default
-  ]
-}
-
-# Outputs
-output "redis_cluster_host" {
-  value = google_redis_cluster.redis_cluster.nodes[0].ip
-}
-
-output "redis_cluster_port" {
-  value = 6379
+output "redis_port" {
+  value = google_redis_instance.redis.port
 }
