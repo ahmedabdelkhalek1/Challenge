5479dcc8421de99914bf794e6307a48e28694204
ahmedabdelkhalek1 <medoooo163@gmail.com>
2025-07-11 17:50:27 +0300

    deploy the Alerts

--- infra/environment/modules/monitoring/main.tf
+++ infra/environment/modules/monitoring/main.tf
@@ -1,12 +1,12 @@
 resource "google_compute_health_check" "app_health_check" {
-  name               = "app-health-check"
-  check_interval_sec = 10
-  timeout_sec        = 5
-  healthy_threshold  = 2
+  name                = "app-health-check"
+  check_interval_sec  = 10
+  timeout_sec         = 5
+  healthy_threshold   = 2
   unhealthy_threshold = 2
 
   http_health_check {
-    port = 80
+    port         = 80
     request_path = "/"
   }
 }
@@ -18,12 +18,12 @@ resource "google_monitoring_alert_policy" "app_unavailable_alert" {
   conditions {
     display_name = "App Health Check Failed"
     condition_threshold {
-      filter = "metric.type=\"compute.googleapis.com/instance/health\" resource.type=\"http_load_balancer\""
-      duration = "60s"
-      comparison = "COMPARISON_GT"
+      filter          = "metric.type=\"compute.googleapis.com/instance/health\" resource.type=\"http_load_balancer\""
+      duration        = "60s"
+      comparison      = "COMPARISON_GT"
       threshold_value = 0
       aggregations {
-        alignment_period = "60s"
+        alignment_period   = "60s"
         per_series_aligner = "ALIGN_RATE"
       }
     }
